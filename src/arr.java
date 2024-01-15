d9b1054618b683de65a75b1a4f7a42149930678b
welcomeglory <153584777+welcomeglory@users.noreply.github.com>
2024-01-15 15:15:41 +0900

    Update arr.java
    change

--- src/arr.java
+++ src/arr.java
@@ -3,10 +3,7 @@ import java.util.Scanner;
 public class arr {
 	public static void main(String[] args) {
 		Scanner sc =  new Scanner(System.in);
-		
-		//dkjhfkjsdf
-		//dbfkdbnf
-		//엏루리후ㅏㄴ앟ㅇㄴ라;ㅣㄴ앙ㄹ,ㅡㄴㅇ라ㅡ
+			
 		System.out.println("welcome");
 	
 		int j= sc.nextInt();
