f66a8736055362eb9ede0a4216c4c6cfc1308f72
sjy <sjy@DESKTOP-3QLVGT6>
2024-01-15 22:15:43 +0900

    up

--- src/UpDown.java
+++ src/UpDown.java
@@ -1,14 +1,24 @@
 public class UpDown {
-	private int[] arrNum = new int[10];
-
+	int i;
+	int num2 = (int) (Math.random() * 99) + 1;
+	private int num1;
 	public UpDown() {
-		int num2;
 	}
 
-	public void getResult(UpDown num) {
-		// for(int i; i<arrNum.length;i++) {
-		// arrNum[i] = num;
-		// }
+	public void getResult(int num1) {
+		while (true) {
+			System.out.println("숫자를 입력해주세요 : ");
+			for (int j = 10; j > 0; j--) {
+				System.out.println(num1 + " " + num2);
+				if (num1 > num2) {
+					System.out.println("Up ===>" + j + "번 남아 있습니다.");
+				} else if (num1 < num2) {
+					System.out.println("Down ===>" + j + "번 남아 있습니다.");
+				} else {
+					System.out.println("일치");
+				}
+			}
+			break;
+		}
 	}
-
 }
