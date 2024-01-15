9238aff899a3c72239d8988c344d71872b8461f5
601-14 <601-14@DESKTOP-30C095C>
2024-01-15 14:40:14 +0900

    first

---
+++ src/arr.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class arr {
+	public static void main(String[] args) {
+		Scanner sc =  new Scanner(System.in);
+		
+		//dkjhfkjsdf
+		int j= sc.nextInt();
+		
+		int[] arr = new int[j];
+		int sum = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = i+1;
+			sum = sum + arr[i];
+		}
+		System.out.println(sum);
+	}
+
+	private static String next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
