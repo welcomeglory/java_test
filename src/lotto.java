a09919c9aa6881c42bc85081cf8c8893d8c33448
601-14 <601-14@DESKTOP-30C095C>
2024-01-15 16:52:20 +0900

    lotto

---
+++ src/lotto.java
@@ -0,0 +1,18 @@
+public class lotto {
+	public static void main(String[] args) {
+		int[] lotto = new int[6];
+
+		for (int i = 0; i < lotto.length; i++) {
+			lotto[i] = (int) (Math.random() * 45) + 1;
+			for (int j = 0; j < i; j++) {
+				if (lotto[i] == lotto[j]) {
+					i--;
+					break;
+				}
+			}
+		}
+		for (int i = 0; i < lotto.length; i++) {
+			System.out.print(lotto[i]+" ");
+		}
+	}
+}
