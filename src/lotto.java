7f52f5a171686546928354676b79aef09641ced0
welcomeglory <godbu2021@naver.com>
2024-02-06 12:24:48 +0900

    1

--- src/lotto.java
+++ src/lotto.java
@@ -1,7 +1,7 @@
 public class lotto {
 	public static void main(String[] args) {
 		int[] lotto = new int[6];
-
+///
 		for (int i = 0; i < lotto.length; i++) {
 			lotto[i] = (int) (Math.random() * 45) + 1;
 			for (int j = 0; j < i; j++) {
